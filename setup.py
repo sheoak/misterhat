a014441f029f7e91d78f00fc009e183084a0aad0
sheoak <sheoak@users.noreply.github.com>
2019-05-07 12:30:22 +0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Misterhat',
+    version='0.1dev',
+    author="Sheoak",
+    url="https://github.com/sheoak/misterhat",
+    author_email="sheoak@users.noreply.github.com",
+    packages=['misterhat', ],
+    license='',
+    description="Playing around with the sense-hat",
+    long_description=open('README.rst').read(),
+)
